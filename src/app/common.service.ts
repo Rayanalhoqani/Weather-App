bdec6aafd82828716be946516b0207bd63a414f0
Rayanalhoqani <144122469+Rayanalhoqani@users.noreply.github.com>
2023-10-18 11:21:38 +0400

    Add files via upload

---
+++ src/app/common.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+  constructor(private http:HttpClient) { }
+
+  getWeatherData(city:string):Observable<any>{
+    var  headers={
+      'X-RapidAPI-Key': 'e7dc1177damshbe3a3c747a130e4p129711jsn984b83666ffd',
+      'X-RapidAPI-Host': 'openweather43.p.rapidapi.com'
+    }
+    var q = city;
+    var appid = ['da0f9c8d90bde7e619c3ec47766a42f4'];
+    var units = 'standard';
+      return this.http.get('https://openweather43.p.rapidapi.com/weather',{params:{q,appid,units },headers:headers})
+  }
+}
